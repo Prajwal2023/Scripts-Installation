db0f42214d64809b56899e8ef752fe3d0ff7a7f9
Prajwal2023 <134365977+Prajwal2023@users.noreply.github.com>
2024-02-12 14:52:28 +0530

    Update Install-awscli.sh
    add new command related to the awscli install

--- Install-awscli.sh
+++ Install-awscli.sh
@@ -2,3 +2,15 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+
+#new INSTALL through Python 3
+#sudo apt-get update
+
+#sudo apt-get install python3-pip
+
+#sudo pip install awscli
+
+#aws --version
+
+#aws configure 
